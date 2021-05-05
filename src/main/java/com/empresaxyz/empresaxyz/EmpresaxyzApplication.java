bf10d50b4040836f0213441e815e2c5b91938b70
Andres Puello <andrespuello53@gmail.com>
2021-05-04 19:50:58 -0500

    agrego dependencias para el layout

--- src/main/java/com/empresaxyz/empresaxyz/EmpresaxyzApplication.java
+++ src/main/java/com/empresaxyz/empresaxyz/EmpresaxyzApplication.java
@@ -1,13 +1,20 @@
 package com.empresaxyz.empresaxyz;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmpresaxyzApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmpresaxyzApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmpresaxyzApplication.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
 }
